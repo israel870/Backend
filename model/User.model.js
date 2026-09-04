e65d72983dfceca672b6918d9fb2cc087d0ad152
israel870 <israeltobiloba24@gmail.com>
2026-09-04 11:23:32 +0100

    Sixth commit

--- model/User.model.js
+++ model/User.model.js
@@ -6,7 +6,7 @@ const userSchema = mongoose.Schema({
     emailAddress:{type: String, required:true, unique:true},
     password:{type: String, required:true},
     role:{type:String, required:true},
-    
+    agreed:{type:Boolean, required:true}
 })
 
 const userModel = mongoose.model('user',userSchema );
