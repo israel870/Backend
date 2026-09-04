fdaae8b3c63de8cc205c45a6a6c80b5e9818431b
israel870 <israeltobiloba24@gmail.com>
2026-09-04 12:57:40 +0100

    Seventh commit

--- controller/staySide.controller.js
+++ controller/staySide.controller.js
@@ -1,40 +1,40 @@
-const studentModel = require('../model/User.model')
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt');
-const userModel = require('../model/User.model');
-const { transporter } = require('../services/nodemailer.service');
+const studentModel = require("../model/User.model");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const userModel = require("../model/User.model");
+const { transporter } = require("../services/nodemailer.service");
 
+const postUser = async (req, res) => {
+  try {
+    console.log(req.body);
 
-const postUser = async (req, res)=>{
-    try {
-        console.log(req.body);
+    const verCode = Math.floor(Math.random() * 1000000)
+      .toString()
+      .padStart(6, "0");
 
-        const salt = await bcrypt.genSalt(10)
-        const hashedPassword = await bcrypt.hash(req.body.password, salt)
-        req.body.password = hashedPassword
-
-        const verCode = Math.floor(Math.random()*1000000).toString().padStart(6,'0');
-        const newUser = new userModel(req.body)
-        const info = await transporter.sendMail({
-            from:`${process.env.SMTP_USER}`,
-            to:`${req.body.emailAddress}`,
-            subject:"Welcome to Stayside",
-            html:`<div><b>Hello</b> ${req.body.firstName} </div>
+    const info = await transporter.sendMail({
+      from: `${process.env.SMTP_USER}`,
+      to: `${req.body.emailAddress}`,
+      subject: "Welcome to Stayside",
+      html: `<div><b>Hello</b> ${req.body.firstName} </div>
             <h3>Your 6 digit code is </h3> ${verCode}
-            `  
-        })  
+            `,
+    });
 
-        const savedUser = await newUser.save()
-       
+    const newUser = new userModel(req.body);
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    req.body.password = hashedPassword;
+    const savedUser = await newUser.save();
 
-    
+    res.status(200).json({ message: "sucess", savedUser });
+  } catch (error) {
+    console.error("Nodemailer Error:", error); // Check your terminal for this!
+  return res.status(400).json({ 
+    message: "Registration failed", 
+    error: error.message || error 
+  });
+  }
+};
 
-        res.status(200).json({"message":"sucess", savedUser})
-    } catch (error) {
-        console.log(error);
-        res.status(400).json({"message":"Bad request", error})
-        
-    }
-}
-
-module.exports = {postUser}
+module.exports = { postUser };
